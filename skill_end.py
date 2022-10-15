497df4035a47790b0b740e40f3a371fefc5f110d
cdfisher <cdfisher96@gmail.com>
2022-10-15 18:08:56 -0400

    Enhancements to end-of-competition raffle draw to make it more robust, code cleanup.

--- skill_end.py
+++ skill_end.py
@@ -23,7 +23,11 @@ win_emoji = [':first_place:', ':second_place:', ':third_place:']
 
 for i in range(WINNERS):
     rsn = df.at[i, 'RSN']
-    msg += '{}: {} XP gained: {:,}\n'.format(win_emoji[i], rsn, df.at[i, 'Gained'])
+    # Handle case in which there are more winners than we have emoji
+    if i < 3:
+        msg += '{}: {} XP gained: {:,}\n'.format(win_emoji[i], rsn, df.at[i, 'Gained'])
+    else:
+        msg += ' {}) : {} XP gained: {:,}\n'.format(i+1, rsn, df.at[i, 'Gained'])
 
 participants = set()
 
@@ -40,27 +44,22 @@ line = '\n' + str(len(participants)) + ' have met the participation ' \
 
 msg += line
 
-# TODO: Remove after testing. Pads participants list in the case there
-# TODO: are fewer players on the participants list than winners of
-# TODO: the participation raffle. Will be handled more cleanly.
-if len(participants) < RAFFLE_WINNERS:
-    participants.add('A')
-    participants.add('B')
-    participants.add('C')
-    participants.add('D')
-    participants.add('E')
-
-par_srtd = sorted(participants)
+par_srtd = sorted(list(participants))
 
 for x in par_srtd:
     msg += x + '\n'
 
-msg += '\nThe winners of the participation prizes are:\n'
+if len(par_srtd) <= RAFFLE_WINNERS:
+    # If there are fewer participants than prize packages, everyon that reached the threshold gets one
+    msg += 'There were enough prizes allotted for everyone listed above as a participant to get one!\n'
+else:
+    # Otherwise draw prizes as normal
+    msg += '\nThe winners of the participation prizes are:\n'
 
-winners = sorted(random.sample(participants, RAFFLE_WINNERS))
+    winners = sorted(random.sample(par_srtd, RAFFLE_WINNERS))
 
-for w in winners:
-    msg += w + '\n'
+    for w in winners:
+        msg += w + '\n'
 
 log_message('Winners selected and raffle prize drawn.')
 

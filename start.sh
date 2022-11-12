108425a283c3e41e7eb24786b01e0c7ee8b02e0e
cdfisher <cdfisher96@gmail.com>
2022-11-11 19:27:15 -0500

    Fixed typo

--- start.sh
+++ start.sh
@@ -28,4 +28,4 @@ sudo chmod -R 777 *.csv # Wildcard is a less than ideal solution so
                         # this will probably be replaced by pulling
                         # a value from a config file in the future
 
-sudo chmod -R 777 *log*.txt # Makes logs accessable for future updates.
+sudo chmod -R 777 *log*.txt # Makes logs accessible for future updates.

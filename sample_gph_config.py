b5e5fb6e2711d753465ee9313bde12776428f80e
cdfisher <cdfisher96@gmail.com>
2022-12-08 18:11:14 -0500

    Updates minor version number

--- sample_gph_config.py
+++ sample_gph_config.py
@@ -8,7 +8,7 @@ Then rename file to 'gph_config.py' before running.
 
 # ---------------Bot info---------------
 # Version number for logging purposes
-GPH_VERSION = 'v0.4.1 beta'
+GPH_VERSION = 'v0.4.2 beta'
 # Bot name to include in JSON payload
 BOT_NAME = 'Gold Partyhat'
 

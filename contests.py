e1a1f5cac0d76f68e50bfd0520b8fb52e68267f7
cdfisher <cdfisher96@gmail.com>
2023-02-21 17:49:21 -0500

    Added missing type hinting and improved docstrings

--- contests.py
+++ contests.py
@@ -6,8 +6,7 @@ import pprint
 
 
 class ContestData:
-    """Represents the settings for a single contest for Gold Partyhat
-    """
+    """Represents the settings for a single contest for Gold Partyhat."""
     def __init__(self, dictionary):
         self.contest_id = dictionary['contest_id']
         self.title = dictionary['title']
@@ -32,14 +31,16 @@ class ContestData:
         self.multi_targets = dictionary['multi_targets']
         return
 
-    def get_all_data(self):
+    def get_all_data(self) -> tuple:
+        """Returns a tuple of all class attributes"""
         return (self.contest_id, self.title, self.mode, self.target, self.threshold, self.units,
                 self.group, self.top_n, self.winners, self.raffle_mode, self.raffle_winners,
                 self.silent, self.start, self.end, self.interval, self.update_number,
                 self.n_participants, self.dynamic_prizes, self.datafile, self.logfile,
                 self.multi_targets)
 
-    def update_entry(self, entry, value):
+    def update_entry(self, entry, value) -> None:
+        """Updates the entry for a given setting"""
         if not hasattr(self, entry):
             raise AttributeError(f'ContestData object {self} with contest id {self.contest_id} '
                                  f'does not have an attribute {entry}. Failed to set {entry} to'
@@ -48,23 +49,28 @@ class ContestData:
             self.__setattr__(entry, value)
         return
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """String representation of object"""
         return str(self.__dict__)
 
-    def __repr__(self):
-        #return str(self.__dict__)
+    def __repr__(self) -> str:
+        """Formatted string representation of object, used when printing
+        a parent ContestTable object as a string, for example
+        """
         return pprint.pformat(self.__dict__, indent=8)
 
 
 class ContestTable:
     """Represents a table of contests in the form of a dictionary of
-    contest_id: ContestData pairs."""
+    contest_id: ContestData pairs.
+    """
     def __init__(self, dictionary):
         self.table = dict()
         for key, value in dictionary.items():
             self.table[key] = ContestData(value)
 
     def get_contest(self, contest_id: str) -> ContestData:
+        """Fetches a contest by contest_id"""
         try:
             _contest = self.table[contest_id]
         except KeyError:
@@ -72,18 +78,29 @@ class ContestTable:
         return _contest
 
     def add_contest(self, data: ContestData) -> None:
+        """Adds a new ContestData object to the table"""
         contest_id = data.__getattribute__('contest_id')
+        # If the contest ID is already in the table, remove the old
+        # entry before adding the new one.
+        if contest_id in self.table:
+            self.remove_contest(contest_id)
         self.table[contest_id] = data
 
     def remove_contest(self, contest_id: str) -> None:
+        """Removes ContestData matching contest_id from the table
+        if it exists.
+        """
         if contest_id in self.table:
             del self.table[contest_id]
 
     def __str__(self) -> str:
+        """Simple string representation of the object."""
         return str(self.table)
 
-    def to_file(self, filename: str):
+    def to_file(self, filename: str) -> None:
+        """Writes a formatted string representation of the object
+        to a text file.
+        """
         with open(filename + '.txt', 'w') as file:
             pstring = pprint.pformat(self.table, indent=4)
-            #file.write(str(self.table))
             file.write(pstring)
